c97993501417144daaf4640920636a0a88c3b783
Manny Singh <likebeats@gmail.com>
2024-01-12 00:39:04 -0500

    setup workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   // Use the name of your repository
-  basePath: '/manny.github.io',
-  assetPrefix: '/manny.github.io/',
+  basePath: "/manny.github.io",
+  // assetPrefix: '/manny.github.io/',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
