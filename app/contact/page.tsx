abf797fb2623d7a408d51f82dba6e2198b460410
Manny Singh <likebeats@gmail.com>
2024-01-12 13:44:07 -0500

    updates to about me and contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,30 @@
+import Image from 'next/image';
+import { REPO_PREFIX } from '../constants';
+
 export default function Contact() {
     return (
-        <div className="flex">
-            <h1>Contact</h1>
+        <div className="flex flex-col text-center pt-16">
+            <h1 className="text-4xl">Get in contact with me</h1>
+            <div className="flex flex-col items-center justify-center space-y-4 m-8 mb-12">
+                <div>317-224-5337</div>
+                <div>
+                    <a href="mailto:likebeats@gmail.com">likebeats@gmail.com</a>
+                </div>
+            </div>
+            <div className="flex justify-center space-x-4">
+                <a href="https://www.linkedin.com/in/likebeats" target="_blank">
+                    <Image src={`${REPO_PREFIX}/images/linkedin-icon.png`} alt="LinkedIn" width={40} height={40} />
+                </a>
+                <a href="https://github.com/likebeats" target="_blank">
+                    <Image src={`${REPO_PREFIX}/images/github-icon.png`} alt="Github" width={40} height={40} />
+                </a>
+                <a href="https://twitter.com/likebeats" target="_blank">
+                    <Image src={`${REPO_PREFIX}/images/twitter-icon.png`} alt="Twitter" width={40} height={40} />
+                </a>
+                <a href="https://angel.co/likebeats" target="_blank">
+                    <Image src={`${REPO_PREFIX}/images/angel-list-icon.png`} alt="AngelList" width={40} height={40} />
+                </a>
+            </div>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,71 @@
 export default function AboutMeHome() {
     return (
-        <div className="flex">
-            <h1>About Me</h1>
+        <div className="flex flex-col">
+            <h1>Manpreet “Manny” Singh</h1>
+            <h4>Full Stack Software Engineer</h4>
+
+            <p>
+                Self-proclaimed hacker junky. Hackathon enthusiast. Creative engineer with a focus on design. Colts fan.
+                Lover of folk music. Favorite language is Swift. Apple & Tesla fanboy.
+            </p>
+
+            <p>
+                I love bringing ideas to life. I have a passion for creating any kind of software, large or small, that
+                helps improve peoples lives. Always hungry to learn about new technology and better ways to do things.
+                In todays world, technology is the center of success and progress. Technology done right can be powerful
+                for an individual/business.
+            </p>
+
+            <br />
+            <h4>Skills</h4>
+
+            <p>
+                Swift · Objective-C · JavaScript · TypeScript · Python · Java · C# · MEAN Stack · LAMP Stack · Node.js ·
+                React.js · Angular · Vue.js · Xamarin · React Native · Apple · Android · HTML5 · CSS3 · Bootstrap · Git
+                · Xcode · Android Studio · Sketch · OOP · FP · MongoDB · PostgreSQL · BLE · AWS
+            </p>
+
+            <br />
+            <h4>Past Hackathons</h4>
+
+            <ul>
+                <li>
+                    First place winner of{' '}
+                    <a
+                        href="https://www.smartfile.com/blog/2013-smartfile-platform-bake-off-the-party-recap/"
+                        target="_blank"
+                    >
+                        2013 SmartFile Platform Bake-Off
+                    </a>{' '}
+                    for creating{' '}
+                    <a href="/#/projects/58f4c0f11915100c44de8b3b" target="_blank">
+                        SmartClip
+                    </a>
+                    , a Google Chrome extension that enables you to capture text, links, and images from articles and
+                    save them for later reading.
+                </li>
+                <li>
+                    First place in{' '}
+                    <a
+                        href="https://www.angieslist.com/news-releases/team-glassworks-cracks-best-code-angie%E2%80%99s-list-inaugural-hack-day.htm"
+                        target="_blank"
+                    >
+                        Angie’s List Hack Day
+                    </a>{' '}
+                    event. Created the first Google Glass app for Angie’s List SnapFix.
+                </li>
+                <li>
+                    Second place winner in{' '}
+                    <a href="https://www.topcoder.com/blog/feedthehack-hackathon/" target="_blank">
+                        FeedTheHack hackathon by Appirio
+                    </a>{' '}
+                    for{' '}
+                    <a href="https://github.com/likebeats/IndyFoodTruck-iOS" target="_blank">
+                        IndyFoodTrucks
+                    </a>
+                    , an iOS app that allowed food truck customers to find their favorite food trucks throughout Indy.
+                </li>
+            </ul>
         </div>
     );
 }
